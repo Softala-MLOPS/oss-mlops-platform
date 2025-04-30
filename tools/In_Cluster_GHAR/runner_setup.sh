57360e37af87d9d1b45af9bef784cf86c0fa976e
makotosoul <minhthien.ng4612@gmail.com>
2025-04-30 13:50:34 +0300

    script draft

---
+++ tools/In_Cluster_GHAR/runner_setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#TODO please add auth
+read -p "please type in your organization: " org
+read -p "please type in your repository's name: " repo
+
+
+echo $org
+echo $repo
+
+token=gh api --method POST  repos/$org/$repo/actions/runners/registration-token
+
+deploy_runner()
+{
+ 	kubectl apply -f github-runner-deployment.yaml
+	echo "Creating kubernetes secret"
+	kubectl create secret generic github-runner-secrets \
+  		--from-literal=GITHUB_TOKEN="$token"\
+  		--from-literal=GITHUB_URL="https://github.com/$org/$repo" \
+  		-n actions-runner
+}
+
+if kubectl delete deployment github-runner --namespace=actions-runner; then
+	kubectl delete secret github-runner-secrets --namespace actions-runner
+	echo "Reinstalling the github-runner"
+	deploy_runner
+else 
+	echo "No in cluster runner was found..."
+	echo "Creating new in cluster runner"
+	deploy_runner
+fi
+
+
