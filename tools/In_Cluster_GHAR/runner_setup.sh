127db6466269496aeb2b13b68bbd9b23e4540046
makotosoul <minhthien.ng4612@gmail.com>
2025-04-30 16:00:04 +0300

    pls fix6

--- tools/In_Cluster_GHAR/runner_setup.sh
+++ tools/In_Cluster_GHAR/runner_setup.sh
@@ -16,7 +16,9 @@ token=$(gh api --method POST repos/$org/$repo/actions/runners/registration-token
 echo $token
 deploy_runner()
 {
-	kubectl create namespace actions-runner
+	if ! [[ $(kubectl get namespace actions-runner) ]]; then
+		kubectl create namespace actions-runner
+	fi
 	echo "Creating kubernetes secret"
 	kubectl create secret generic github-runner-secrets --from-literal=GITHUB_TOKEN=$token --from-literal=GITHUB_URL="https://github.com/$org/$repo" -n actions-runner
  	kubectl apply -f github-runner-deployment.yaml
@@ -24,17 +26,21 @@ deploy_runner()
 }
 
 if kubectl delete deployment github-runner --namespace=actions-runner; then
-	kubectl delete namespace actions-runner
-	kubectl delete secret github-runner-secrets --namespace actions-runner
-	echo "Reinstalling the github-runner"
-	deploy_runner
+	if kubectl delete secret github-runner-secrets --namespace actions-runner; then
+			echo "Reinstalling the github-runner"
+			deploy_runner
+	else
+		echo "Reinstalling the github-runner"
+		deploy_runner
 else 
-	kubectl delete namespace actions-runner ||
-	{
+	if kubectl delete secret github-runner-secrets --namespace actions-runner; then
 		echo "No in cluster runner was found..."
 		echo "Creating new in cluster runner"
 		deploy_runner
-	}
+	else
+		echo "No in cluster runner was found..."
+		echo "Creating new in cluster runner"
+		deploy_runner
 fi
 
 
