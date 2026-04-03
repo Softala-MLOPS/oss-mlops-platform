2ef2198d7a7b1777f09ec7f09e8380970fd38239
Anh Tran <bhm349@myy.haaga-helia.fi>
2026-04-03 22:52:45 +0300

    Added Kubeflow host initialization and client creation with host parameter

--- tools/CLI-tool/files/development/src/submit_run.py
+++ tools/CLI-tool/files/development/src/submit_run.py
@@ -1,6 +1,7 @@
 # submit_run.py
 import kfp
 import sys
+import os
 
 sys.path.append('../src')
 from pipelines.pipeline_definitions.pipeline_definition import pipeline
@@ -8,9 +9,13 @@ from pipelines.pipeline_arg.pipeline_arg import arguments
 
 def submit_pipeline():
 
-    client = kfp.Client() 
-    
+    # 1. Get the Kubeflow address from environment variables (Default is None if not set)
+    kfp_host = os.environ.get("KFP_HOST")
 
+    # 2. Initialize the client with the specific host
+    # If kfp_host = None, it will fall back to the default behavior (looking for ~/.kube/config)
+    client = kfp.Client(host=kfp_host) 
+    
     # Define your experiment and run name
     experiment_name = "demo-experiment"
     run_name = "demo-run-through-github-actions-on-OSS-MLOps-platform-in-development-environment"
